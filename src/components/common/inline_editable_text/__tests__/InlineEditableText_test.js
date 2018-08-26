7a9d5d6b7deb2df7f07e08c5bfd2b0068bd951f6
mageets <mageets@hotmail.com>
2018-08-26 18:34:21 +0100

    added test

---
+++ src/components/common/inline_editable_text/__tests__/InlineEditableText_test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {InlineEditableText} from '../InlineEditableText';
+
+describe('InlineEditableText', () => {
+    it('some test', ()=>{
+        expect(true).toEqual(true);
+    });
+});
